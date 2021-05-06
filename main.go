dba42749fbd70a425f7645fa439773e33c4152aa
Thiago Souza <thiago@elastic.co>
2021-05-06 12:25:17 +0100

    refactor package

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/tsouza/terraform-provider-elasticstack/internal/provider"
+	"github.com/tsouza/terraform-provider-elasticstack/elasticstack/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
